82c81468342bfecad0f6017b4cf5755829547fa1
Mujjamil <jamadarmj5@gmail.com>
2026-06-27 11:25:43 +0530

    leetcode dsa: 1672  solved richest-customer-wealth problem of leetcode'

---
+++ src/com/mj/oops/linearsearch/MaxWealth.java
@@ -0,0 +1,32 @@
+package com.mj.oops.linearsearch;
+
+public class MaxWealth {
+    public static void main(String[] args){
+
+        int[][] accounts ={
+                {1,5},
+                {7,3},
+                {3,5}
+        };
+        System.out.println(maxWealth(accounts));
+    }
+    public static int maxWealth(int[][] accounts){
+        //person = row
+        //account = col
+        int ans = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++) {
+            //when you start a new column , take a new sum for that row
+            int sum = 0;
+            for (int account = 0; account < accounts[person].length; account++) {
+            sum += accounts[person][account];
+            }
+            //now we have sum of accounts of person
+            //check with overall answer
+            if(sum>ans){
+                ans = sum;
+            }
+        }
+        return ans;
+
+    }
+}
