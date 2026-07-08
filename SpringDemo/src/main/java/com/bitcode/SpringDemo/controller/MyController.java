c57a79e5b0351a9940d8e8963fc7ac8a38d3b31b
Mujjamil <jamadarmj5@gmail.com>
2026-07-08 12:20:57 +0530

    class session:learning about springboot getmapping methods

--- SpringDemo/src/main/java/com/bitcode/SpringDemo/controller/MyController.java
+++ SpringDemo/src/main/java/com/bitcode/SpringDemo/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.bitcode.SpringDemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,17 @@ public class MyController {
     public String getHomePage(){
         return "Hello its Home Page";
     }
+    @GetMapping("/products")
+    public String  getProducts(){
+        return "All product fetched";
+    }
+    //localhost:8080/search?pid=1&pCompany=abc
+    @GetMapping("/search")
+    public String search(@RequestParam int pid,@RequestParam String pName){
+        {
+            return "Product id = "+pid+"Brand Name="+pName;
+        }
+
+    }
+
 }
