69676717e91647c0b718a91e3efaebfe17511113
Mujjamil <jamadarmj5@gmail.com>
2026-08-14 09:24:45 +0530

    done with doubleylinkedlist

--- src/com/vishalsirdsa/DoubleyLinkedList/Main.java
+++ src/com/vishalsirdsa/DoubleyLinkedList/Main.java
@@ -3,6 +3,7 @@ package com.vishalsirdsa.DoubleyLinkedList;
 public class Main {
     public static void main(String[] args){
         DoubleyLinkedList dll = new DoubleyLinkedList();
+
         dll.insert(10);
         dll.display();
         dll.insert(20);
@@ -14,13 +15,13 @@ public class Main {
         dll.insert(50);
         dll.display();
 
-       dll.insert(60 , 1);
-       dll.display();
-       dll.insert(70,5);
-       dll.display();
-       dll.insert(80, 8);
-       dll.display();
-       boolean isInserted = dll.insert(90,10);
+        dll.insert(60, 1);
+        dll.display();
+        dll.insert(70,5);
+        dll.display();
+        dll.insert(80, 8);
+        dll.display();
+        boolean isInserted = dll.insert(90, 10);
         System.out.println(isInserted);
         dll.display();
 
