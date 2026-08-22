a8a1bf96e1cfd7f826c352e0ff0b21c07cf23471
Mujjamil <jamadarmj5@gmail.com>
2026-08-22 11:27:51 +0530

    learning new methods of strings

---
+++ src/com/javascript/datatypes/String/methodsofstring.js
@@ -0,0 +1,9 @@
+let text = "afafdskassdfa";
+console.log(text.length);//length gives length of string
+console.log(text.charAt(1))//returns character by its index
+console.log(text.charCodeAt(3));//return character code of that index 
+console.log(text.codePointAt(3));//give character point code 
+console.log(text.at(4));//return character of that position 
+
+
+
