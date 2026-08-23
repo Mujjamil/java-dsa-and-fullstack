16c94d018a9968299ac06258312136c7ef662428
Mujjamil <jamadarmj5@gmail.com>
2026-08-23 11:18:10 +0530

    done with the methods of strings

--- src/com/javascript/datatypes/String/methodsofstring.js
+++ src/com/javascript/datatypes/String/methodsofstring.js
@@ -4,6 +4,28 @@ console.log(text.charAt(1))//returns character by its index
 console.log(text.charCodeAt(3));//return character code of that index 
 console.log(text.codePointAt(3));//give character point code 
 console.log(text.at(4));//return character of that position 
+console.log(text.toUpperCase());//Make upper side 
+console.log(text.toLowerCase());//Make lower side
+
+
+
+
+
+
+let text1 = "Mujjamil";
+let text2 = "Jamadar";
+console.log(text1.concat(" ",text2));// concat merge two strings
+
+let txt = "Mujjamil , Apple , Banana";
+//console.log(txt.slice(7,13));
+console.log(txt.trim());
+
+let txx = "The quick brown fox";
+console.log(txx.split(""));
+const myArr = txx.split(" ");
+console.log(myArr);
+
+
 
 
 
