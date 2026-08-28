9a4ced22cf002f88e27b9ee65bfecc607d5dcb09
Mujjamil <jamadarmj5@gmail.com>
2026-08-28 20:23:04 +0530

    started learning arrays with coverting array to string and also about arrays characterstics

---
+++ src/com/javascript/arrays/index.js
@@ -0,0 +1,12 @@
+const carts = ["bmw","mercedes","lamborgini"];
+
+const cars = [];//empty array
+cars[0] = "MBW";
+cars[1] = "Mercedes";
+cars[2] = "Lamborgini";
+
+cars[0] = "Volkswagn"//changing the array by. index value
+
+console.log(cars[0]);//accessing the array
+
+
