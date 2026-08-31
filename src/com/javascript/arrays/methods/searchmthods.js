92970a85e855aee3ab0f2940d2e9d186151e051f
Mujjamil <jamadarmj5@gmail.com>
2026-08-31 11:44:05 +0530

    done with sorting and searching methods

---
+++ src/com/javascript/arrays/methods/searchmthods.js
@@ -0,0 +1,5 @@
+const fruits = ["Apple","Mango","Banana","Orange"];
+let position = fruits.indexOf("Mango")//gives the index of fruit
+let lastIndex = fruits.lastIndexOf("Orange");//gives the last index of array
+let include = fruits.includes("Mango")
+console.log(include);
