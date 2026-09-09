cfada3a230a6548a98f225a8e9bd6c1a3babf0e4
Mujjamil <jamadarmj5@gmail.com>
2026-09-09 10:04:50 +0530

    done with the datatype

---
+++ python/datatype.py
@@ -0,0 +1,25 @@
+# list = ["Mujjamil","Jamadar",2,3,4]
+# print(list)
+# print(list[3])
+# print(list[2])
+# list[1] = ["Jamadaar"];
+# print(
+#     list
+# )
+# #list are mutable ordered collection used to store mutliple value into single variable.
+
+
+# tuplee = ("Mujjamil","Jamadar",5,6,7);
+# print(tuplee);
+# print(tuplee[1])
+# tuplee[0] = ("MJ") 
+# print(tuplee)
+# #tuples are immutable and ordered collection used to store multiple value in single variable 
+# #tuples cannot be change
+
+
+settt = {"Mujjamil","Jamadar",5,323,2};
+print(settt);
+for i in settt:
+    print(i)
+#sets are mutable but unordered collection which store element if we want to access elements in sets we have to iteratre over them.
