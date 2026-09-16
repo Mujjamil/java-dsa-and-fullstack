bbe9145c468749401828799cd6e461db9c677754
Mujjamil <jamadarmj5@gmail.com>
2026-09-16 18:20:38 +0530

    learning input output file system with vishal sir

---
+++ src/com/javabyvishalsir/demohandson/FileUtil.java
@@ -0,0 +1,11 @@
+//package com.javabyvishalsir.demohandson;
+//
+//public class FileUtil {
+//    1)function to delete a file , delete will access as an argument (string path)
+//        2)publi static boolean move(String sourcepath , String deletePath)
+//
+//        3)want you to write your own pushback input stream
+//        4)sequence input stream
+//            inside sequnce stema you can pass array of
+//
+//}
