7282e6df9b9858ce1e11b25250c0039b353f5975
Mujjamil <jamadarmj5@gmail.com>
2026-09-17 15:40:11 +0530

    done with arrays'

--- src/com/definelabpractice/practice.java
+++ src/com/definelabpractice/practice.java
@@ -1,15 +1,28 @@
 package com.definelabpractice;
 
+import java.util.Arrays;
+
 public class practice {
   public static void main(String[] args){
 //      int a = 10;
 //      int b = 10;
 //      System.out.println(a == b);//output will be true
 
-      String s1 = new String("Hello");
-      String s2 = new String("Hello");
-      System.out.println(s1 == s2);//output will be false
-      System.out.println(s1.equals(s2));// output will be true
+//      String s1 = new String("Hello");
+//      String s2 = new String("Hello");
+//      System.out.println(s1 == s2);//output will be false
+//      System.out.println(s1.equals(s2));// output will be true
+
+
+
+      int[] arr = new int[5];
+      arr[0] = 5;
+      arr[1] = 3;
+      arr[2] = 4;
+
+      String[] name = {"Mujjamil","Saurabh","Tushar"};
+      System.out.println(Arrays.toString(arr));//have to apply to string then it will print
+      System.out.println(Arrays.toString(name));// have to apply to string method then it will print
   }
 
 }
