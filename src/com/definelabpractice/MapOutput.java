3cc197b01e79771839433fb8e2186eda79b27c33
Mujjamil <jamadarmj5@gmail.com>
2026-09-18 12:09:11 +0530

    done with hashmap qeustion

---
+++ src/com/definelabpractice/MapOutput.java
@@ -0,0 +1,11 @@
+package com.definelabpractice;
+import java.util.HashMap;
+import java.util.Map;
+public class MapOutput {
+    public static void main(String[] args){
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"Mujjamil");
+        map.put(1,"Jamadar");
+        System.out.println(map);
+    }
+}
