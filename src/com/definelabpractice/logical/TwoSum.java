475a3d73288f601dceca962869f387aa0c2a8f4a
Mujjamil <jamadarmj5@gmail.com>
2026-09-21 13:32:58 +0530

    done with two some qeuestions solution properly

--- src/com/definelabpractice/logical/TwoSum.java
+++ src/com/definelabpractice/logical/TwoSum.java
@@ -3,21 +3,20 @@ package com.definelabpractice.logical;
 import java.util.HashMap;
 
 public class TwoSum {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] arr = {2, 7, 11, 15};
         int target = 9;
-
         HashMap<Integer , Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
-            int complement = target - arr[i];//what number do we need to complete the pair?
+            int complement = target - arr[i];
             if(map.containsKey(complement)){
-                System.out.println("Values "+complement +" "+arr[i]);
-                 System.out.println("Indices "+ map.get(complement) +","+i);
+                System.out.println("Values "+complement+arr[i]);
+                System.out.println("Indices "+map.get(complement)+" , "+i);
                 return;
             }
             map.put(arr[i],i);
         }
-        System.out.println("No two sum solution found");
+        System.out.println("No two some solution found");
     }
 
 }
