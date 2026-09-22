54812035d459752e4cb00caece251fe1e8ccb256
Mujjamil <jamadarmj5@gmail.com>
2026-09-22 10:08:12 +0530

    learning math mehtods

---
+++ src/com/javascript/math/Math.js
@@ -0,0 +1,24 @@
+//A Math object allows you to perform mathematical operations
+
+console.log(Math.round(4.9));//returns the nearest integer.
+console.log(Math.sign(8));//
+console.log(Math.pow(8,2));//used for power
+console.log(Math.sqrt(64));//return the square root of X.
+
+console.log(Math.abs(-4.7));//gives positive value.
+
+let num = Math.min(0, 150, 30, 20, -8, -200);//gives minimum value
+let num2 = Math.max(0, 150, 30, 20, -8, -200);//gives maximum value 
+console.log(num);
+console.log(num2);
+
+
+let random = Math.random();
+console.log(random);
+
+let log = Math.log(10)
+console.log(log);
+
+
+
+
