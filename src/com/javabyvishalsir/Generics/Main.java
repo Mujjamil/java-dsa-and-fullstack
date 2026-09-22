2e14431122d30a7655c1c2972e86ecd555b2138d
Mujjamil <jamadarmj5@gmail.com>
2026-09-22 15:54:11 +0530

    done with the pair clas

--- src/com/javabyvishalsir/Generics/Main.java
+++ src/com/javabyvishalsir/Generics/Main.java
@@ -2,15 +2,21 @@ package com.javabyvishalsir.Generics;
 
 public class Main {
     public static void main(String[] args){
-        Holder<Integer> s1 = new Holder<>(234);
-        System.out.println(s1.getValue());
-        s1.setValue(345);
-        System.out.println(s1.getValue());
+//        Holder<Integer> s1 = new Holder<>(234);
+//        System.out.println(s1.getValue());
+//        s1.setValue(345);
+//        System.out.println(s1.getValue());
+//
+//        Holder<String> s2 = new Holder<>("Mujjamil");
+//        System.out.println(s2.getValue());
+//        s2.setValue("Tejas");
+//        System.out.println(s2.getValue());
+//        ------------------------------------------------------------------------
 
-        Holder<String> s2 = new Holder<>("Mujjamil");
-        System.out.println(s2.getValue());
-        s2.setValue("Tejas");
-        System.out.println(s2.getValue());
+        Pair<Integer , String> p1 = new Pair<>(12,"Mujjamil");
+        System.out.println(p1);
+        System.out.println(p1.getKey());
+        System.out.println(p1.getValue());
 
 
     }
